5057035670e35900677c6dca7b3b6193eb89ae2c
elen-oz <ozerova.lenka@gmail.com>
2024-01-16 18:13:09 +0100

    feat: add styles to Product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,31 +1,36 @@
-import { useCart } from "../providers/CartProvider/CartProvider.hook";
+import { useCart } from '../providers/CartProvider/CartProvider.hook';
+import styles from './Product.module.css';
 
 function Product({ product }) {
   const { name, price, image } = product;
   const { add, remove } = useCart();
 
   return (
-    <div style={{ padding: "2em" }}>
-      <img width={200} height={200} src={image} alt={name} />
-      <p style={{ textDecoration: "none" }}>
+    <div className={styles.card}>
+      <img src={image} alt={name} />
+      <p className={styles.title}>
         {name} - {price} SEK
       </p>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          add(product.id, 1);
-        }}
-      >
-        + Add to cart
-      </button>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          remove(product.id, 1);
-        }}
-      >
-        - Remove from cart
-      </button>
+      <div className={styles.buttonsContainer}>
+        <button
+          className={styles.button}
+          onClick={(e) => {
+            e.preventDefault();
+            add(product.id, 1);
+          }}
+        >
+          + Add
+        </button>
+        <button
+          className={styles.button}
+          onClick={(e) => {
+            e.preventDefault();
+            remove(product.id, 1);
+          }}
+        >
+          - Remove
+        </button>
+      </div>
     </div>
   );
 }
