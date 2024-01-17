e7bc09e855c70e8090fdbe1aad7b2612c71d75f9
thetylerwolf <tylernicholaswolf@gmail.com>
2024-01-17 14:33:22 +0100

    fix to itemPage

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 
 import useGetProduct from "../hooks/useGetProduct";
 import styles from "./ItemPage.module.css";
+import useCart from "../hooks/useCart";
 
 export default function ItemPage() {
   const { id } = useParams();
   const { product } = useGetProduct(id);
+  const { add } = useCart();
   const item = product;
 
   if (!item) return <div>Item not found</div>;
@@ -18,7 +20,9 @@ export default function ItemPage() {
       <p className={styles.title}>
         {name} - {price} SEK
       </p>
-      <button className={styles.button}>+ Add to cart</button>
+      <button className={styles.button} onClick={() => add(id, 1)}>
+        + Add to cart
+      </button>
       <div className={styles.description}>{description}</div>
     </div>
   );
