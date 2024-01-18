1aadaa5c19abd67a2f7d1102b4b16d4d6c777020
thetylerwolf <tylernicholaswolf@gmail.com>
2024-01-18 22:40:54 +0100

    url fix

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@ const localAPIUrl = process.env.REACT_APP_LOCAL_API_URL;
 const prodAPIUrl = process.env.REACT_APP_PROD_API_URL;
 
 export const backendURL =
-  process.env.NODE_ENV === "development" ? prodAPIUrl : prodAPIUrl;
+  process.env.NODE_ENV === "development" ? localAPIUrl : prodAPIUrl;
 
 console.log(process.env.NODE_ENV, process.env, backendURL);
